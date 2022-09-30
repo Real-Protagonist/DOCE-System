67168aa9ee05af318ebcbc1a30bef4a607aa92a6
Mesaque Txangala <nikimanuel15@gmail.com>
2022-09-30 19:43:23 +0100

    Implementação do form principal e de Cad. funcionarios

---
+++ Athena/cadastroFuncionarios.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace Athena
+{
+    partial class cadastroFuncionarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pbFoto = new System.Windows.Forms.PictureBox();
+            this.lblFoto = new System.Windows.Forms.Label();
+            this.btnImportarFoto = new System.Windows.Forms.Button();
+            this.txtPNome = new System.Windows.Forms.TextBox();
+            this.txtUNome = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbSexo = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtTel2 = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtFuncao = new System.Windows.Forms.TextBox();
+            this.dtNascimento = new System.Windows.Forms.DateTimePicker();
+            this.dtCadastro = new System.Windows.Forms.DateTimePicker();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.dgFuncionariosView = new System.Windows.Forms.DataGridView();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnEditarFuncionario = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFoto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgFuncionariosView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(352, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CADASTRAR FUNCIONÁRIO";
+            // 
+            // pbFoto
+            // 
+            this.pbFoto.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbFoto.Location = new System.Drawing.Point(27, 65);
+            this.pbFoto.Name = "pbFoto";
+            this.pbFoto.Size = new System.Drawing.Size(155, 170);
+            this.pbFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbFoto.TabIndex = 1;
+            this.pbFoto.TabStop = false;
+            // 
+            // lblFoto
+            // 
+            this.lblFoto.AutoSize = true;
+            this.lblFoto.ForeColor = System.Drawing.Color.Gray;
+            this.lblFoto.Location = new System.Drawing.Point(53, 238);
+            this.lblFoto.Name = "lblFoto";
+            this.lblFoto.Size = new System.Drawing.Size(101, 13);
+            this.lblFoto.TabIndex = 2;
+            this.lblFoto.Text = "Foto do Funcionário";
+            // 
+            // btnImportarFoto
+            // 
+            this.btnImportarFoto.Location = new System.Drawing.Point(56, 254);
+            this.btnImportarFoto.Name = "btnImportarFoto";
+            this.btnImportarFoto.Size = new System.Drawing.Size(98, 23);
+            this.btnImportarFoto.TabIndex = 3;
+            this.btnImportarFoto.Text = "Importar";
+            this.btnImportarFoto.UseVisualStyleBackColor = true;
+            // 
+            // txtPNome
+            // 
+            this.txtPNome.Location = new System.Drawing.Point(228, 86);
+            this.txtPNome.Name = "txtPNome";
+            this.txtPNome.Size = new System.Drawing.Size(184, 20);
+            this.txtPNome.TabIndex = 4;
+            // 
+            // txtUNome
+            // 
+            this.txtUNome.Location = new System.Drawing.Point(435, 86);
+            this.txtUNome.Name = "txtUNome";
+            this.txtUNome.Size = new System.Drawing.Size(184, 20);
+            this.txtUNome.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(228, 137);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(136, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(225, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Primeiro Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(432, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Último Nome";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(225, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Tel.1";
+            // 
+            // cbSexo
+            // 
+            this.cbSexo.FormattingEnabled = true;
+            this.cbSexo.Items.AddRange(new object[] {
+            "Masculino",
+            "Feminino"});
+            this.cbSexo.Location = new System.Drawing.Point(642, 86);
+            this.cbSexo.Name = "cbSexo";
+            this.cbSexo.Size = new System.Drawing.Size(121, 21);
+            this.cbSexo.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(639, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Sexo";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(432, 121);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Tel.2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(639, 121);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Email";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(225, 179);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Função";
+            // 
+            // txtTel2
+            // 
+            this.txtTel2.Location = new System.Drawing.Point(435, 137);
+            this.txtTel2.Name = "txtTel2";
+            this.txtTel2.Size = new System.Drawing.Size(136, 20);
+            this.txtTel2.TabIndex = 15;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(642, 137);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(184, 20);
+            this.txtEmail.TabIndex = 16;
+            // 
+            // txtFuncao
+            // 
+            this.txtFuncao.Location = new System.Drawing.Point(228, 195);
+            this.txtFuncao.Name = "txtFuncao";
+            this.txtFuncao.Size = new System.Drawing.Size(184, 20);
+            this.txtFuncao.TabIndex = 17;
+            // 
+            // dtNascimento
+            // 
+            this.dtNascimento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtNascimento.Location = new System.Drawing.Point(786, 86);
+            this.dtNascimento.Name = "dtNascimento";
+            this.dtNascimento.Size = new System.Drawing.Size(102, 20);
+            this.dtNascimento.TabIndex = 18;
+            this.dtNascimento.Value = new System.DateTime(2022, 9, 30, 0, 0, 0, 0);
+            // 
+            // dtCadastro
+            // 
+            this.dtCadastro.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtCadastro.Location = new System.Drawing.Point(435, 195);
+            this.dtCadastro.Name = "dtCadastro";
+            this.dtCadastro.Size = new System.Drawing.Size(102, 20);
+            this.dtCadastro.TabIndex = 19;
+            this.dtCadastro.Value = new System.DateTime(2022, 9, 30, 0, 0, 0, 0);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(783, 70);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(104, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Data de Nascimento";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(432, 179);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(90, 13);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Data de Cadastro";
+            // 
+            // dgFuncionariosView
+            // 
+            this.dgFuncionariosView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgFuncionariosView.BackgroundColor = System.Drawing.Color.Gainsboro;
+            this.dgFuncionariosView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgFuncionariosView.Location = new System.Drawing.Point(228, 254);
+            this.dgFuncionariosView.Name = "dgFuncionariosView";
+            this.dgFuncionariosView.Size = new System.Drawing.Size(894, 278);
+            this.dgFuncionariosView.TabIndex = 22;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadastrar.Location = new System.Drawing.Point(614, 193);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(96, 30);
+            this.btnCadastrar.TabIndex = 23;
+            this.btnCadastrar.Text = "CADASTRAR";
+            this.btnCadastrar.UseVisualStyleBackColor = false;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.LightGray;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Location = new System.Drawing.Point(826, 193);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(96, 30);
+            this.btnCancelar.TabIndex = 24;
+            this.btnCancelar.Text = "CANCELAR";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            // 
+            // btnEditarFuncionario
+            // 
+            this.btnEditarFuncionario.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnEditarFuncionario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditarFuncionario.Location = new System.Drawing.Point(721, 193);
+            this.btnEditarFuncionario.Name = "btnEditarFuncionario";
+            this.btnEditarFuncionario.Size = new System.Drawing.Size(96, 30);
+            this.btnEditarFuncionario.TabIndex = 25;
+            this.btnEditarFuncionario.Text = "EDITAR";
+            this.btnEditarFuncionario.UseVisualStyleBackColor = false;
+            // 
+            // cadastroFuncionarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1191, 554);
+            this.Controls.Add(this.btnEditarFuncionario);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.dgFuncionariosView);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.dtCadastro);
+            this.Controls.Add(this.dtNascimento);
+            this.Controls.Add(this.txtFuncao);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtTel2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbSexo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtUNome);
+            this.Controls.Add(this.txtPNome);
+            this.Controls.Add(this.btnImportarFoto);
+            this.Controls.Add(this.lblFoto);
+            this.Controls.Add(this.pbFoto);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "cadastroFuncionarios";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "cadastroFuncionarios";
+            ((System.ComponentModel.ISupportInitialize)(this.pbFoto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgFuncionariosView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pbFoto;
+        private System.Windows.Forms.Label lblFoto;
+        private System.Windows.Forms.Button btnImportarFoto;
+        private System.Windows.Forms.TextBox txtPNome;
+        private System.Windows.Forms.TextBox txtUNome;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbSexo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtTel2;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtFuncao;
+        private System.Windows.Forms.DateTimePicker dtNascimento;
+        private System.Windows.Forms.DateTimePicker dtCadastro;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.DataGridView dgFuncionariosView;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnEditarFuncionario;
+    }
+}
